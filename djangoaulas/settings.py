82b71301992cec0ab96aa431f2e7e15f760c6604
João garcia <joaogarciadelimaneto@gmail.com>
2020-02-21 11:08:11 -0300

    Instalado Python Decouple
    Adicionada pasta de exemplo da configuração das variaveis
    
    closes #16

--- djangoaulas/settings.py
+++ djangoaulas/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -23,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(i=%=834^yf9%jd!0^y^fe6+0iq@4(=e_2b40oy_u$)zcfoi)1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
