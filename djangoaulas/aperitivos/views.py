0109541070cedcfa27cc6e7a9acb94fee0ac4817
João garcia <joaogarciadelimaneto@gmail.com>
2020-02-22 16:41:37 -0300

    configuração com o banco de dados completa. criação dos testes com e sem migrações configuração do admin no modulo de aperitivos
    closes #44

--- djangoaulas/aperitivos/views.py
+++ djangoaulas/aperitivos/views.py
@@ -1,29 +1,12 @@
-from django.shortcuts import render
-from django.urls import reverse
-
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
-
-videos = [
-    Video('motivacao', 'Video Aperitivo: Motivação', 393131909),
-    Video('instalacao-windows', 'Instalação no windows', 393131909),
-]
-
-videos_dct = {v.slug: v for v in videos}
+from django.shortcuts import render, get_object_or_404
+from djangoaulas.aperitivos.models import Video
 
 
 def video(request, slug):
-    video = videos_dct[slug]
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
 
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
