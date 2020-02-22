fe2c70ec18cc6769d3ba9f97d75c1f92c565504f
João garcia <joaogarciadelimaneto@gmail.com>
2020-02-22 10:21:20 -0300

    Entrega da landing page com inscrição na lista de emails.
    closes 32

--- djangoaulas/urls.py
+++ djangoaulas/urls.py
@@ -17,11 +17,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from djangoaulas.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', include('djangoaulas.base.urls')),
 ]
 
 
