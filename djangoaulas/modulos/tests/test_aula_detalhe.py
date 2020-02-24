cf6ce27b429e62a9c2ab5a3a7ee1dbf35cdbfaa1
João garcia <joaogarciadelimaneto@gmail.com>
2020-02-24 10:53:38 -0300

    Criado breadcrumb Refeita a busca com select related
    closes #54

--- djangoaulas/modulos/tests/test_aula_detalhe.py
+++ djangoaulas/modulos/tests/test_aula_detalhe.py
@@ -24,3 +24,7 @@ def resp(client, aula):
 
 def test_titulo(resp, aula: Aula):
     assert_contains(resp, f'src="https://player.vimeo.com/video/{ aula.vimeo_id }"')
+
+
+def test_modulo_breadcrumb(resp, modulo: Modulo):
+    assert_contains(resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{modulo.titulo}</a></li>')
