47b3a4b2792128740dcff4b416d54b36986f7196
João garcia <joaogarciadelimaneto@gmail.com>
2020-02-24 14:42:10 -0300

    Criada pagina de indice com os modulos e aulas
    closes #58

--- djangoaulas/modulos/views.py
+++ djangoaulas/modulos/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 from djangoaulas.modulos import facade
 
 
+def indice(request):
+    ctx = {'modulos': facade.listar_modulos_com_aulas()}
+    return render(request, 'modulos/indice.html', ctx)
+
+
 def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
