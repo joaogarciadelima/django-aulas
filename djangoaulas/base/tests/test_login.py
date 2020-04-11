ac24f39073dc2dad345c1ac4d5d83c27daeac7a8
joaogarciadelima <joaogarciadelimaneto@gmail.com>
2020-04-11 12:50:44 -0300

    Configurado login

--- djangoaulas/base/tests/test_login.py
+++ djangoaulas/base/tests/test_login.py
@@ -29,4 +29,4 @@ def resp_post(client, usuario):
 
 def test_login_redirect(resp_post):
     assert resp_post.status_code == 302
-    assert resp_post.url == reverse('modulos:indice')
+    assert resp_post.url == reverse('base:home')
