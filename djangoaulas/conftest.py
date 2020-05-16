ebaf3fceb10e17c657cdc6b64aa99be2c4ff7d15
joaogarciadelima <joaogarciadelimaneto@gmail.com>
2020-05-16 12:05:32 -0300

    Implementados testes de usuario logado

---
+++ djangoaulas/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from model_mommy import mommy
+
+
+@pytest.fixture
+def usuario_logado(db, django_user_model):
+    usuario_modelo = mommy.make(django_user_model)
+    usuario_modelo.save()
+    return usuario_modelo
+
+
+@pytest.fixture
+def client_com_usuario_logado(usuario_logado, client):
+    client.force_login(usuario_logado)
+    return client
